242962dce8c7ccc5898ca5c50b2966813444bf89
kkivg <62174869+kkivg@users.noreply.github.com>
2024-03-01 09:57:02 +0800

    Update gabs.py

--- gabs.py
+++ gabs.py
@@ -1,8 +1,34 @@
+'''
+_______________#########_______________________
+______________############_____________________
+______________#############____________________
+_____________##__###########___________________
+____________###__######_#####__________________
+____________###_#######___####_________________
+___________###__##########_####________________
+__________####__###########_####_______________
+________#####___###########__#####_____________
+_______######___###_########___#####___________
+_______#####___###___########___######_________
+______######___###__###########___######_______
+_____######___####_##############__######______
+____#######__#####################_#######_____
+____#######__##############################____
+___#######__######_#################_#######___
+___#######__######_######_#########___######___
+___#######____##__######___######_____######___
+___#######________######____#####_____#####____
+____######________#####_____#####_____####_____
+_____#####________####______#####_____###______
+______#####______;###________###______#________
+________##_______####________####______________
+author：kkivg
+'''
 import sys
 import math
 import re
 
-# 请求用户输入日志文件的名称
+
 name = input("Please enter the log file name: ")
 
 try:
@@ -15,11 +41,11 @@ try:
     nstate = 0
     elevec = []
     magvec = []
-    f_values = []  # 用于存储每个态的f值
-    r_velocities = []  # 用于存储每个态的R(velocity)值
-    current_r_velocities = []  # 临时存储当前状态的R(velocity)值
+    f_values = []  
+    r_velocities = [] 
+    current_r_velocities = []  #
 
-    # 正则表达式匹配 "Excited State" 行来提取f值
+
     pattern = re.compile(r'Excited State.*?f=([\d.]+)')
 
     for line in lines:
@@ -73,7 +99,7 @@ try:
             current_r_velocities = []
             continue
         elif r_velocity_found:
-            # 提取每行的第五个数字并保存
+
             parts = line.split()
             if len(parts) >= 5:
                 try:
@@ -81,15 +107,15 @@ try:
                 except ValueError:
                     print(f"Warning: Could not convert R(velocity) to float in line: {line.strip()}")
 
-    # 输出态的数目，默认输出所有的态
+
     for i in range(nstate):
         elelist = elevec[i]
         maglist = magvec[i]
-        f_value = f_values[i]  # 获取对应的f值
+        f_value = f_values[i] 
         r_velocity_s = r_velocities[0][i]
 
 
-        # 计算相关物理量
+
         X1, Y1, Z1 = elelist[1:4]
         X2, Y2, Z2 = maglist[1:4]
         absmiu_au = (X1**2 + Y1**2 + Z1**2)**0.5
@@ -98,8 +124,8 @@ try:
         absm = absm_au * 9.27400968*0.1  # unit 10^-20 erg/G
         dotmium = X1 * X2 + Y1 * Y2 + Z1 * Z2
         costheta = dotmium / (absmiu_au * absm_au)
-        theta = math.acos(costheta)  # 解为弧度
-        theta_degrees = math.degrees(theta)  # 转换为度
+        theta = math.acos(costheta) 
+        theta_degrees = math.degrees(theta) 
         g = 4*costheta*absmiu*(1E-20)*absm*(1E-20)/((absmiu*(1E-20))**2+(absm*(1E-20))**2)
         R_length = -(
             X1 * 2.541746 * 100 * Y1 * 9.27400968 * 0.1 +
@@ -107,9 +133,9 @@ try:
             Y2 * 2.541746 * 100 * Z2 * 9.27400968 * 0.1
         )
 
-        # 输出结果
+
         print("state     %d" % (i + 1))
-        print("f         %f" % f_value)  # 输出对应的f值
+        print("f         %f" % f_value)  
         print("R         %f"% r_velocity_s)
         print("absmiu    %f 10^-20 esu cm" % absmiu)
         print("absm      %f 10^-20 erg/G" % absm)
